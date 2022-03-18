c46cec19e27253093610268572d659745b7121f1
JustAida <suradet40@gmail.com>
2022-03-18 20:39:49 +0700

    Fix bugs and add get_pegs method

--- mastermind.rb
+++ mastermind.rb
@@ -1,12 +1,6 @@
-# Make Mastermind module that contains 3 classes: Game, Player, Computer
+require "pry-byebug"
 
-# Game class will display the board that contains 12 rows with 4 holes each.
-# It will be played 12 turns, and it'll give information after each guess.
-# Information: Red Pegs indicate correct positions and colours while,
-#   White Pegs indicate correct colours but not the positions.
-# The game will stop if the guess is equal to the secret code or run out of guesses.
-# The secret code will be 4 colours out of 6 colours, for example [R-G-B-P].
-# The colours are [R, G, B, P, Y, O] -> [Red, Green, Blue, Pink, Yellow, Orange].
+# Make Mastermind module that contains 3 classes: Game, Player, Computer
 
 # Player class will have to guess the code until it matches or run out of guesses.
 # It can also decide whether to be the codemaker or codebreaker.
@@ -18,6 +12,13 @@
 # If codemaker then it'll choose the secret code randomly.
 
 module Mastermind
+  # Game class will display the board that contains 12 rows with 4 holes each.
+  # It will be played 12 turns, and it'll give information after each guess.
+  # Information: Red Pegs indicate correct positions and colours while,
+  #   White Pegs indicate correct colours but not the positions.
+  # The game will stop if the guess is equal to the secret code or run out of guesses.
+  # The secret code will be 4 colours out of 6 colours, for example [R-G-B-P].
+  # The colours are [R, G, B, P, Y, O] -> [Red, Green, Blue, Pink, Yellow, Orange].
   class Game
     # Red, Green, Blue, Pink, Yellow, Orange
     COLOURS = %w[R G B P Y O]
@@ -26,14 +27,17 @@ module Mastermind
 
     def initialize(codemaker, codebreaker)
       @board = Array.new(12) { Array.new(4) }
+      @board_str = ""
       @turn = 0
+      # @pegs contain an Array of @red_pegs and @white_pegs for each turn.
+      @pegs = []
       @red_pegs = 0
       @white_pegs = 0
       @codemaker = codemaker
       @codebreaker = codebreaker
     end
 
-    def introduction
+    def intro
       puts "Welcome to Mastermind game!"
       puts "When guessing the code or making the code you should type only 4 colours."
       puts "For example, rgbp or RGBP."
@@ -41,38 +45,60 @@ module Mastermind
     end
 
     def play
-      introduction
+      intro
       # secret_code = @codemaker.get_secret_code UNCOMMENT THIS LATER
       puts "The codemaker has typed in the code."
 
-      until turn == 12
+      until turn == 2
         puts "\nGuess the code."
         # guess = @codebreaker.get_guess UNCOMMENT THIS LATER
         guess = "rgbp" # DELETE THIS AFTER
         update_board(guess)
         display
-        break
+        self.turn += 1
       end
     end
 
     def display
-      puts ""
+      get_pegs("yyrb", "ryyy") if turn == 0 # DELETE THIS AFTER
+      get_pegs("yyrb", "yyyy") if turn == 1 # DELETE THIS AFTER
+
+      puts "\nGuesses | Red Pegs | White Pegs |"
       @board.each_with_index do |holes, index|
-        break if index > turn
+        next unless index == turn
 
         holes.each_with_index do |hole, hole_index|
-          hole.nil? ? print(".") : print(hole)
-          hole_index == 3 ? print("\n") : print("-")
+          @board_str += hole.nil? ? "." : hole
+          @board_str += hole_index == 3 ? " |    #{@pegs[turn][0]}     |     #{@pegs[turn][1]}      |\n" : "-"
         end
       end
-
-      puts "\nRed Pegs: #{red_pegs}"
-      puts "White Pegs: #{white_pegs}"
+      puts @board_str
     end
 
     def update_board(guess)
       guess = guess.upcase.chars
       @board[turn] = guess
+    end
+
+    def get_pegs(secret_code, guess)
+      secret_code = secret_code.upcase.chars
+      guess = guess.upcase.chars
+      secret_code_dup = secret_code[0..-1]
+      guess_dup = guess[0..-1]
+
+      # Check for red pegs.
+      @red_pegs = 0
+      secret_code_dup.each_with_index do |colour, index|
+        if colour == guess[index]
+          secret_code_dup[index] = nil
+          guess_dup[index] = nil
+          @red_pegs += 1
+        end
+      end
+
+      # Check for white pegs.
+      @white_pegs = secret_code_dup.select { |colour| guess_dup.include?(colour) && !colour.nil? }.size
+      @pegs[turn] = [@red_pegs, @white_pegs]
     end
 
     # May have to change later.
@@ -84,3 +110,4 @@ end
 
 game = Mastermind::Game.new("", "")
 game.play
+
